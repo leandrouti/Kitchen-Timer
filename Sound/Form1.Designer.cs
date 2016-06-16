063380398e61a5af28c130aacb2a05d713792ed5
Utiyama Tatsuya Leandro <leandrouti@yahoo.com.br>
2016-06-16 10:59:07 +0900

    Initial Commit

---
+++ Sound/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Sound
+{
+	partial class Timer
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button1.Location = new System.Drawing.Point(60, 157);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(131, 60);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(72, 70);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 19);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(178, 70);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(44, 19);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Set";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(70, 110);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 22);
+			this.label1.TabIndex = 4;
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 75);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(50, 12);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Seconds:";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(16, 14);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "30 Min";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click_1);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(97, 14);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "10 Min";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(178, 14);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 8;
+			this.button5.Text = "3 Min";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// Timer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(266, 243);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Timer";
+			this.Text = "Timer";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+	}
+}
+
---
+++ Sound/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Sound
+{
+	public partial class Timer : Form
+	{
+		private int time;
+		public Timer()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			textBox1.Text = time.ToString();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if(timer1.Enabled == false)
+			{
+				timer1.Enabled = true;
+				button1.Text = "Stop";
+			}else
+			{
+				timer1.Enabled = false;
+				button1.Text = "Start";
+				
+			}
+			
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+
+			if(time <= 0)
+			{
+				SoundPlayer snd = new SoundPlayer(@"c:\windows\media\Alarm01.wav");
+				snd.Play();
+				timer1.Enabled = false;
+				button1.Text = "Start";
+				MessageBox.Show("Time up!");
+				time = Int32.Parse(textBox1.Text);
+				label1.Text = textBox1.Text;
+					
+			}else
+			{
+				time--;
+				 
+			}
+			label1.Text = time.ToString();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			timer1.Enabled = false;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+			int result;
+
+			if (Int32.TryParse(textBox1.Text, out result)) { 
+				time = result;
+				label1.Text = result.ToString();
+			}else
+			{
+				MessageBox.Show("Only numbers are allowed");
+			}
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click_1(object sender, EventArgs e)
+		{
+			textBox1.Text = 1800.ToString();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = 600.ToString();
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = 18.ToString();
+		}
+	}
+}
